7c3467135ff7d5097addbf000b60cfaae2a1b5c5
ben <ben@37441250-4157-47a9-a2c9-f9fe3212d2d3>
2006-03-31 02:52:18 +0000

    now builds in release mode.
    git-svn-id: http://publicsvn.songbirdnest.com/vendor/trunk@290 37441250-4157-47a9-a2c9-f9fe3212d2d3

--- dependencies/vendor/sqlite/songbird_sqlite_configure.sh
+++ dependencies/vendor/sqlite/songbird_sqlite_configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-./configure --prefix=$HOME \
+./configure --prefix=$HOME/_built_sqlite3 \
+  --enable-releasemode \
   --enable-threadsafe \
   --enable-tempstore \
   --enable-cross-thread-connections \
