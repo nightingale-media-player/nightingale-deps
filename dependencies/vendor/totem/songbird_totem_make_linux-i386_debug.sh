8f49f1e24427a51762c6e97b544b5f543c63117f
steve <steve@37441250-4157-47a9-a2c9-f9fe3212d2d3>
2006-07-28 02:35:55 +0000

    Move everything into one script
    git-svn-id: http://publicsvn.songbirdnest.com/vendor/trunk@1590 37441250-4157-47a9-a2c9-f9fe3212d2d3

--- dependencies/vendor/totem/songbird_totem_make_linux-i386_debug.sh
+++ dependencies/vendor/totem/songbird_totem_make_linux-i386_debug.sh
@@ -3,8 +3,21 @@
 # target architecture
 export SB_ARCH=linux
 
-# debus or release
+# debug or release
 export SB_CONFIGURATION=debug
 
+make clean
+rm -f config.log config.status Makefile
+
 . songbird_totem_make_linux-common.sh
+
+make clean
+rm -f config.log config.status Makefile
+
+export SB_CONFIGURATION=release
+
+. songbird_totem_make_linux-common.sh
+
+make clean
+rm -f config.log config.status Makefile
 
