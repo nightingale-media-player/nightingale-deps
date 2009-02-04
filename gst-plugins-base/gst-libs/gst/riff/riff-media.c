df626ed8c7035d5a95ffdbbfe6ecc0ef4e520529
msmith <msmith@37441250-4157-47a9-a2c9-f9fe3212d2d3>
2009-02-04 19:22:06 +0000

    Bug 14620 - Fix up handling of WMA lossless. Also fixes bug 13661
    git-svn-id: http://publicsvn.songbirdnest.com/vendor/trunk@10386 37441250-4157-47a9-a2c9-f9fe3212d2d3

--- gst-plugins-base/gst-libs/gst/riff/riff-media.c
+++ gst-plugins-base/gst-libs/gst/riff/riff-media.c
@@ -1100,6 +1100,7 @@ gst_riff_create_audio_caps (guint16 codec_id,
     case GST_RIFF_WAVE_FORMAT_WMAV1:
     case GST_RIFF_WAVE_FORMAT_WMAV2:
     case GST_RIFF_WAVE_FORMAT_WMAV3:
+    case GST_RIFF_WAVE_FORMAT_WMAV3_L:
     {
       gint version = (codec_id - GST_RIFF_WAVE_FORMAT_WMAV1) + 1;
 
@@ -1109,8 +1110,12 @@ gst_riff_create_audio_caps (guint16 codec_id,
       caps = gst_caps_new_simple ("audio/x-wma",
           "wmaversion", G_TYPE_INT, version, NULL);
 
-      if (codec_name)
-        *codec_name = g_strdup_printf ("WMA Version %d", version + 6);
+      if (codec_name) {
+        if (codec_id == GST_RIFF_WAVE_FORMAT_WMAV3_L)
+          *codec_name = g_strdup ("WMA Lossless");
+        else
+          *codec_name = g_strdup_printf ("WMA Version %d", version + 6);
+      }
 
       if (strf != NULL) {
         gst_caps_set_simple (caps,
@@ -1121,26 +1126,6 @@ gst_riff_create_audio_caps (guint16 codec_id,
             "bitrate", GST_TYPE_INT_RANGE, 0, G_MAXINT, NULL);
       }
       break;
-    }
-    case GST_RIFF_WAVE_FORMAT_WMAV3_L:
-    {
-      /* WMA Version 9 Lossless */
-      caps = gst_caps_new_simple ("audio/x-wma", "wmaversion", G_TYPE_INT, 4, NULL);
-      block_align = TRUE;
-
-      if (codec_name)
-        *codec_name = g_strdup ("WMA Version 3 Lossless");
-
-      if (strf != NULL) {
-        gst_caps_set_simple (caps,
-            "bitrate", G_TYPE_INT, strf->av_bps * 8,
-            "depth", G_TYPE_INT, strf->size, NULL);
-      } else {
-        gst_caps_set_simple (caps,
-            "bitrate", GST_TYPE_INT_RANGE, 0, G_MAXINT, NULL);
-      }
-      break;
-
     }
     case GST_RIFF_WAVE_FORMAT_SONY_ATRAC3:
       caps = gst_caps_new_simple ("audio/x-vnd.sony.atrac3", NULL);
