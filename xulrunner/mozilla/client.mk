d4db8ca1364d627cc0ccb765df3c87fe57a7fd63
preed <preed@37441250-4157-47a9-a2c9-f9fe3212d2d3>
2009-04-14 20:45:18 +0000

    Bug 16041: merge in upstream xulrunner version 1.9.0.8 into the songbird vendor tree; test-compiled on Linux x86_64. Merged revisions 10478 via svnmerge from https://svn.songbirdnest.com/vendor/upstream/xulrunner
    git-svn-id: http://publicsvn.songbirdnest.com/vendor/trunk@10525 37441250-4157-47a9-a2c9-f9fe3212d2d3

--- xulrunner/mozilla/client.mk
+++ xulrunner/mozilla/client.mk
@@ -410,11 +410,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = FIREFOX_3_0_7_RELEASE
-NSPR_CO_TAG          = FIREFOX_3_0_7_RELEASE
-NSS_CO_TAG           = FIREFOX_3_0_7_RELEASE
-LDAPCSDK_CO_TAG      = FIREFOX_3_0_7_RELEASE
-LOCALES_CO_TAG       = FIREFOX_3_0_7_RELEASE
+MOZ_CO_TAG           = FIREFOX_3_0_8_RELEASE
+NSPR_CO_TAG          = FIREFOX_3_0_8_RELEASE
+NSS_CO_TAG           = FIREFOX_3_0_8_RELEASE
+LDAPCSDK_CO_TAG      = FIREFOX_3_0_8_RELEASE
+LOCALES_CO_TAG       = FIREFOX_3_0_8_RELEASE
 
 #######################################################################
 # Defines
---
+++ xulrunner/mozilla/toolkit/components/satchel/test/satchel_common.js
@@ -0,0 +1,74 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Satchel Test Code.
+ *
+ * The Initial Developer of the Original Code is
+ * Ehsan Akhgari.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Ehsan Akhgari <ehsan.akhgari@gmail.com> (Original Author)
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+ 
+/*
+ * $_
+ *
+ * Returns the element with the specified |name| attribute.
+ */
+function $_(formNum, name) {
+  var form = document.getElementById("form" + formNum);
+  if (!form) {
+    ok(false, "$_ couldn't find requested form " + formNum);
+    return null;
+  }
+
+  var element = form.elements.namedItem(name);
+  if (!element) {
+    ok(false, "$_ couldn't find requested element " + name);
+    return null;
+  }
+
+  // Note that namedItem is a bit stupid, and will prefer an
+  // |id| attribute over a |name| attribute when looking for
+  // the element.
+
+  if (element.hasAttribute("name") && element.getAttribute("name") != name) {
+    ok(false, "$_ got confused.");
+    return null;
+  }
+
+  return element;
+}
+
+function cleanUpFormHist() {
+  netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+  var formhist = Components.classes["@mozilla.org/satchel/form-history;1"].
+                 getService(Components.interfaces.nsIFormHistory2);
+  formhist.removeAllEntries();
+}
+cleanUpFormHist();
