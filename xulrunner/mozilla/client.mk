3ef589855574f41efaa2c80028c791944a96283d
preed <preed@37441250-4157-47a9-a2c9-f9fe3212d2d3>
2010-01-05 20:58:46 +0000

    Merged revisions 10998 via svnmerge from https://svn.songbirdnest.com/vendor/upstream/xulrunner
    ........
      r10998 | preed | 2010-01-05 12:50:45 -0800 (Tue, 05 Jan 2010) | 1 line
      
      Vendor drop of xulrunner 1.9.0.17/Firefox 3.0.17
    ........
    
    
    git-svn-id: http://publicsvn.songbirdnest.com/vendor/trunk@10999 37441250-4157-47a9-a2c9-f9fe3212d2d3

--- xulrunner/mozilla/client.mk
+++ xulrunner/mozilla/client.mk
@@ -409,11 +409,11 @@ MODULES_all :=                                  \
 #
 # For branches, uncomment the MOZ_CO_TAG line with the proper tag,
 # and commit this file on that tag.
-MOZ_CO_TAG           = FIREFOX_3_0_16_RELEASE
-NSPR_CO_TAG          = FIREFOX_3_0_16_RELEASE
-NSS_CO_TAG           = FIREFOX_3_0_16_RELEASE
-LDAPCSDK_CO_TAG      = FIREFOX_3_0_16_RELEASE
-LOCALES_CO_TAG       = FIREFOX_3_0_16_RELEASE
+MOZ_CO_TAG           = FIREFOX_3_0_17_RELEASE
+NSPR_CO_TAG          = FIREFOX_3_0_17_RELEASE
+NSS_CO_TAG           = FIREFOX_3_0_17_RELEASE
+LDAPCSDK_CO_TAG      = FIREFOX_3_0_17_RELEASE
+LOCALES_CO_TAG       = FIREFOX_3_0_17_RELEASE
 
 #######################################################################
 # Defines
