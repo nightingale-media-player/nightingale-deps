05cd8a02fa253568c704f5c840c26ed692c33e1d
Matt Parnell <parwok@gmail.com>
2011-09-21 02:56:36 -0500

    perfecting the setup so even a caveman (or lazy coder like myself) could do it

--- build-xulrunner.sh
+++ build-xulrunner.sh
@@ -1,3 +1,10 @@
+# NOTE: If you've already built once successfully, then don't use this
+# script to repackage! Instead go to xulrunner-$XUL and 
+# make -f Makefile.songbird xr-packaging
+
+# XUL Version
+XUL="6.0.2"
+
 # output directory
 if [ ! -d linux\-`arch` ]; then
     mkdir linux\-`arch`
@@ -8,10 +15,40 @@ if [ ! -d build ] ; then
     mkdir build
 fi
 
+# xul source directory
+if [ ! -d "xulrunner-$XUL/mozilla" ] ; then
+    mkdir "xulrunner-$XUL/mozilla"
+fi
+
+# get the source files if this is a fresh build or new version
+if [ ! -f "xulrunner-$XUL/xulrunner-$XUL.source.tar.bz2" ] ; then
+	cd "xulrunner-$XUL"
+	
+	wget "https://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$XUL/source/xulrunner-$XUL.source.tar.bz2"
+	
+	tar xvf "xulrunner-$XUL.source.tar.bz2"
+	
+	# just in case the directory exists already
+	rm -rf mozilla
+	
+	mv "mozilla-release" mozilla
+	
+	cd ../../
+else
+	cd "xulrunner-$XUL"
+	
+	rm -rf mozilla
+	
+	tar xvf "xulrunner-$XUL.source.tar.bz2"
+	
+	mv "mozilla-release" mozilla
+
+fi
+
 # build path ...just because we're lazy 
 # and don't want to rewrite this whole build system
 export SB_VENDOR_BUILD_ROOT=`pwd`
 
 # change this if we do a version bump
-cd xulrunner-6.0.2
+cd "xulrunner-$XUL"
 make -f Makefile.songbird
