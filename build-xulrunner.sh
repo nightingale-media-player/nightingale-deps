bf36f03208dea2452d588ea468b0baa2557039b7
Matt Parnell <parwok@gmail.com>
2011-09-22 22:25:06 -0500

    use HG or a tarball to build

--- build-xulrunner.sh
+++ build-xulrunner.sh
@@ -4,6 +4,10 @@
 
 set -e
 
+# Use HG or TARBALL to fetch the sources?
+# Really, just use whichever suits you or is fastest
+USE="TARBALL"
+
 # XUL Version
 XUL="6.0.2"
 
@@ -24,26 +28,50 @@ get_bundle() {
     }
     mv mozilla-release.hg mozilla-release.old.hg
   }
+  # if you have axel or aria2c installed, i'd suggest using it here to speed up the download
   wget -O mozilla-release.hg ftp://ftp.mozilla.org/pub/mozilla.org/firefox/bundles/mozilla-release.hg
 }
 
-[ -d mozilla ] || {
-  # mozilla directory does not exist; get the source code
-  mkdir -p mozilla
-  hg --cwd mozilla init
-  # make sure we have a bundle available
-  [ -f mozilla-release.hg ] || get_bundle
-  # unbundle
-  hg --cwd mozilla unbundle ../mozilla-release.hg
-  # make sure we have the release we want
-  hg --cwd mozilla tags | grep -F "FIREFOX_${XUL//./_}_RELEASE" || {
-    # perhaps the bundle was too old; grab a new one
-    get_bundle
+use_hg() {
+  [ -d mozilla ] || {
+    # mozilla directory does not exist; get the source code
+    mkdir -p mozilla
+    hg --cwd mozilla init
+    # make sure we have a bundle available
+    [ -f mozilla-release.hg ] || get_bundle
+    # unbundle
     hg --cwd mozilla unbundle ../mozilla-release.hg
+    # make sure we have the release we want
+    hg --cwd mozilla tags | grep -F "FIREFOX_${XUL//./_}_RELEASE" || {
+      # perhaps the bundle was too old; grab a new one
+      get_bundle
+      hg --cwd mozilla unbundle ../mozilla-release.hg
+    }
+    # remember to checkout
+    hg co "FIREFOX_${XUL//./_}_RELEASE"
   }
-  # remember to checkout
-  hg co "FIREFOX_${XUL//./_}_RELEASE"
 }
+
+use_tarball() {
+	if [ ! -f "xulrunner-$XUL.source.tar.bz2" ] ; then	
+	    # if you have axel or aria2c installed, i'd suggest using it here to speed up the download	
+		wget "https://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$XUL/source/xulrunner-$XUL.source.tar.bz2"
+		tar xvf "xulrunner-$XUL.source.tar.bz2"
+		# just in case the directory exists already
+		rm -rf mozilla
+		mv "mozilla-release" mozilla
+	else
+		rm -rf mozilla
+		tar xvf "xulrunner-$XUL.source.tar.bz2"
+		mv "mozilla-release" mozilla
+	fi
+}
+
+if [ $USE = "HG" ] ; then
+	use_hg
+elif [ $USE = "TARBALL" ] ; then
+	use_tarball
+fi
 
 make -f Makefile.songbird
 
