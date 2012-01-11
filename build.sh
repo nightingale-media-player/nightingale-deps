454b0837b7985025fe97e752eb512a25cce4572d
Matt Parnell <parwok@gmail.com>
2012-01-10 22:21:14 -0800

    setup system to build the official songbird xulrunner (1.9.2)

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ set -e
 USE="TARBALL"
 
 # XUL Version
-export XUL="6.0.2"
+export XUL="1.9.2"
 
 # Top level build path
 export SB_VENDOR_BUILD_ROOT="$(cd "$(dirname "$0")" && pwd)"
@@ -18,60 +18,7 @@ export SB_VENDOR_BUILD_ROOT="$(cd "$(dirname "$0")" && pwd)"
 mkdir -p "linux-$(arch)"
 
 cd "xulrunner"
-# just in case the directory exists already
-#rm -rf mozilla
-
-get_bundle() {
-  [ -f mozilla-release.hg ] && {
-    [ -f mozilla-release.old.hg ] && {
-      rm mozilla-release.old.hg
-    }
-    mv mozilla-release.hg mozilla-release.old.hg
-  }
-  # if you have axel or aria2c installed, i'd suggest using it here to speed up the download
-  wget -O mozilla-release.hg ftp://ftp.mozilla.org/pub/mozilla.org/firefox/bundles/mozilla-release.hg
-}
-
-use_hg() {
-  [ -d mozilla ] || {
-    # mozilla directory does not exist; get the source code
-    mkdir -p mozilla
-    hg --cwd mozilla init
-    # make sure we have a bundle available
-    [ -f mozilla-release.hg ] || get_bundle
-    # unbundle
-    hg --cwd mozilla unbundle ../mozilla-release.hg
-    # make sure we have the release we want
-    hg --cwd mozilla tags | grep -F "FIREFOX_${XUL//./_}_RELEASE" || {
-      # perhaps the bundle was too old; grab a new one
-      get_bundle
-      hg --cwd mozilla unbundle ../mozilla-release.hg
-    }
-    # remember to checkout
-    hg co "FIREFOX_${XUL//./_}_RELEASE"
-  }
-}
-
-use_tarball() {
-	if [ ! -f "xulrunner-$XUL.source.tar.bz2" ] ; then	
-	    # if you have axel or aria2c installed, i'd suggest using it here to speed up the download	
-		wget "https://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$XUL/source/xulrunner-$XUL.source.tar.bz2"
-		tar xvf "xulrunner-$XUL.source.tar.bz2"
-		# just in case the directory exists already
-		rm -rf mozilla
-		mv "mozilla-release" mozilla
-	else
-		rm -rf mozilla
-		tar xvf "xulrunner-$XUL.source.tar.bz2"
-		mv "mozilla-release" mozilla
-	fi
-}
-
-if [ $USE = "HG" ] ; then
-	use_hg
-elif [ $USE = "TARBALL" ] ; then
-	use_tarball
-fi
+svn co "http://publicsvn.songbirdnest.com/vendor/trunk/xulrunner-${XUL}/mozilla/"
 
 # fix for kernels > 3.X on versions of xul without security setup for them
 case $OSTYPE in
@@ -87,5 +34,3 @@ esac
 
 # build Xulrunner
 make
-
-cp -a "$SB_VENDOR_BUILD_ROOT/oldscripts/"* "$SB_VENDOR_BUILD_ROOT/linux-$(arch)/mozilla-$XUL/scripts"
