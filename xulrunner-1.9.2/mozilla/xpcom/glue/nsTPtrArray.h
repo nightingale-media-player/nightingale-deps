b2a09775bd919f421b0847c64c486f022c6e169d
Matt Parnell/ilikenwf <parwok@gmail.com>
2012-09-22 12:45:19 -0500

    mozilla build fixes in xpcom, fixes for newer libpngs in libpr0n

--- xulrunner-1.9.2/mozilla/xpcom/glue/nsTPtrArray.h
+++ xulrunner-1.9.2/mozilla/xpcom/glue/nsTPtrArray.h
@@ -64,7 +64,7 @@ class nsTPtrArray : public nsTArray<E*> {
 
     // Initialize this array and pre-allocate some number of elements.
     explicit nsTPtrArray(size_type capacity) {
-      SetCapacity(capacity);
+      this->SetCapacity(capacity);
     }
     
     // The array's copy-constructor performs a 'deep' copy of the given array.
