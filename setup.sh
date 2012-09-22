3f18b06c4c3849615f0c631aaa5da303ab2059b5
matt <matt@nightingale-builder.localdomain>
2012-09-22 01:44:07 -0500

    setup the setup script so it actually works when "source setup.sh" is called

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+
+export DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 export CC=gcc-4.7
-export SB_VENDOR_BUILD_ROOT=$(pwd)
+export SB_VENDOR_BINARIES_CO_ROOT=$DIR
+export SB_VENDOR_BUILD_ROOT=$DIR
