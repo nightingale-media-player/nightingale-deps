ff928e36e1861252228ede1c37ee2d36ead8d9ba
ilikenwf <parwok@gmail.com>
2012-12-26 15:12:44 -0600

    Revert "not viable"
    This reverts commit 580e2900447d3722bd13ba1ae740ae8bccdf4ac1.

--- songbird-vendor-defs.mk
+++ songbird-vendor-defs.mk
@@ -227,6 +227,7 @@ ifeq (Darwin,$(SB_VENDOR_ARCH))
   SB_CFLAGS += -gstabs+ \
                -isysroot $(MACOSX_SDK) \
                -D__MACOSX__ \
+			   -L /opt/local/lib \
                -D__APPLE__
 
   SB_LDFLAGS += -headerpad_max_install_names -isysroot $(MACOSX_SDK) \
