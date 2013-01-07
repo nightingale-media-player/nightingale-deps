b69ed1e62518fa91fe21efc1f6bb929581924502
ilikenwf <parwok@gmail.com>
2013-01-07 12:44:10 -0600

    mac fixes, proper build.sh support in a proper, sane order

--- build.sh
+++ build.sh
@@ -56,7 +56,39 @@ case $OSTYPE in
 			mkdir "build"
 		fi
 
-		make
+        echo -e "Building gettext..."
+        make -C gettext -f Makefile.songbird
+        echo -e "Building glib..."
+        make -C glib -f Makefile.songbird
+        echo -e "Building libidl..."
+        make -C libidl -f Makefile.songbird
+        echo -e "Building flac..."
+        make -C flac -f Makefile.songbird
+        echo -e "Building libjpeg..."
+        make -C libjpeg -f Makefile.songbird
+        echo -e "libogg gettext..."
+        make -C libogg -f Makefile.songbird
+        echo -e "Building libtheora..."
+        make -C libtheora -f Makefile.songbird
+        echo -e "Building libtool..."
+        make -C libtool -f Makefile.songbird
+        echo -e "Building libvorbis..."        
+        make -C libvorbis -f Makefile.songbird
+        echo -e "Building sqlite..."
+        make -C sqlite -f Makefile.songbird
+        echo -e "Building taglib..."        
+        make -C taglib -f Makefile.songbird
+        echo -e "Building xulrunner and crossing our fingers..."
+        make -C xulrunner-1.9.2 -f Makefile.songbird xr-all
+        echo -e "Building gstreamer bits..."
+        make -C gstreamer -f Makefile.songbird
+        echo -e "Building gst plugins...here's hoping these all build!"
+        make -C gst-plugins-base -f Makefile.songbird
+        make -C gst-plugins-good -f Makefile.songbird
+        make -C gst-plugins-bad -f Makefile.songbird
+        make -C gst-plugins-ugly -f Makefile.songbird
+        echo "Done!"
+
     ;;
     msys*)
 		if [ ! -d "windows-i686-msvc8" ]; then
