2072b1e454904b51d313242d948231e6dbe23a4c
John Murray <johnmurrayvi@gmail.com>
2013-06-12 12:36:08 -0500

    rename script, add sqlite script in case I ever just want to build sqlite again... which will probably be never..

--- depbuild.sh
+++ depbuild.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+ARCH="$(uname -m)"
+
 # XUL Version
 export XUL="9.0.1"
 
-# Top level build path
-export SB_VENDOR_BUILD_ROOT="$(cd "$(dirname "$0")" && pwd)"
+export DIR="$(cd "$(dirname "$0")" && pwd)"
+export SB_VENDOR_BINARIES_CO_ROOT=$DIR
+export SB_VENDOR_BUILD_ROOT=$DIR
+export SB_TARGET_ARCH="linux-x86_64"
 
+mkdir -p "checkout/linux-$ARCH"
+
+# output directory
+mkdir -p "linux-$ARCH"
+
+rm -rf build
+mkdir build
 
 cd xulrunner
-
 # fix for kernels > 3.X on versions of xul without security setup for them
 case $OSTYPE in
     linux*)
@@ -20,10 +30,28 @@ case $OSTYPE in
         # weee! xulrunner already has security coreconf for our kernel!
     ;;
 esac
+cd ../
 
-# build Xulrunner
-make xr-all
+echo -e "Building xulrunner...\n"
+make -C xulrunner xr-all
+
+echo -e "Building sqlite...\n"
+make -C sqlite -f Makefile.songbird
+
+echo -e "Building taglib...\n"
+make -C taglib -f Makefile.songbird
+
+if [ ! -d "linux-$ARCH/mozilla-$XUL/debug" ] ; then 
+    cd "linux-$ARCH/mozilla-$XUL"
+    mkdir debug
+    mv bin frozen idl include lib scripts debug/
+    cd ../../
+fi
+
+if [ ! -d "linux-$ARCH/xulrunner-$XUL/debug" ] ; then
+    mkdir "linux-$ARCH/xulrunner-$XUL/debug"
+    mv "linux-$ARCH/xulrunner-$XUL/xulrunner.tar.bz" "linux-$ARCH/xulrunner-$XUL/debug"
+fi
 
 cp -a "oldscripts/*" "linux-$ARCH/mozilla-$XUL/debug/scripts/"
 cp -a "oldscripts/*" "linux-$ARCH/mozilla-$XUL/release/scripts/"
-
