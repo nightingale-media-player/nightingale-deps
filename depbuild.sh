6f16d12cc0498d72c8ddad72d3bdd3c87e43f001
John Murray <johnmurrayvi@gmail.com>
2014-02-12 19:24:12 -0500

    fix DIR export
    Works on msys, need to double check that this doesn't break linux builds

--- depbuild.sh
+++ depbuild.sh
@@ -5,7 +5,7 @@ ARCH="$(uname -m)"
 # XUL Version
 export XUL="9.0.1"
 
-export DIR="$(cd "$(dirname "$0")" && pwd)"
+export DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export SB_VENDOR_BINARIES_CO_ROOT=$DIR
 export SB_VENDOR_BUILD_ROOT=$DIR
 
