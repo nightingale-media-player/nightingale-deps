82bc41c5b812e4112f7723e72da19563e8f190af
John Murray <johnmurrayvi@gmail.com>
2014-07-18 14:32:01 -0500

    unset the exec permissions on xulrunner libs

--- xulrunner-1.9.2/make-xulrunner-tarball.sh
+++ xulrunner-1.9.2/make-xulrunner-tarball.sh
@@ -45,6 +45,7 @@ $FIND -L "$permFixDir" -perm +0111 -not -type d -not -name '*\.so' -exec chmod 0
 
 notice "finding non executable files..."
 $FIND -L "$permFixDir" -not -perm +0111 -exec chmod 0644 {} \; -print
+$FIND -L "$permFixDir" -perm +0111 -name '*\.so' -exec chmod 0644 {} \; -print
 
 cd "$permFixDir"
 
