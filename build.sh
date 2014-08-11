f6fff616087f605c79f1f7a42332642fe58d8faf
John Murray <johnmurrayvi@gmail.com>
2014-08-11 15:53:02 -0500

    lots of fixes/changes for build.sh
    MSYS: add specific build order
    MSYS: script-ify using updated gettext with prebuilt libiconv & prebuilt glib
    MSYS: script-ify using bins package on windows for gstreamer* 1.0+
    Darwin: move up XR in build order to prevent failure when finding needed script
    unify indentation
    don't clobber build dir every run (especially if one dep fails to build)
    separately check <outputdir> and checkout/<outputdir>

--- build.sh
+++ build.sh
@@ -9,51 +9,52 @@ export SB_VENDOR_BUILD_ROOT=$DIR
 # Currently, we build both debug and release, which takes a lot longer,
 # especially on xulrunner... TODO: make it optional to build debug
 
-rm -rf build
-mkdir build
+if [ ! -d "build" ]; then
+    mkdir "build"
+fi
 
 case $OSTYPE in
-	# linux is easy, as all it requires right now is xulrunner, sqlite, and taglib
-	# we'll get to a point where this is unnecessary on linux altogether in the future
-	linux*)
-		export CXXFLAGS="-fpermissive"
-		export CFLAGS=$CXXFLAGS
-		export CCFLAGS=$CXXFLAGS
+    # linux is easy, as all it requires right now is xulrunner, sqlite, and taglib
+    # we'll get to a point where this is unnecessary on linux altogether in the future
+    linux*)
+        export CXXFLAGS="-fpermissive"
+        export CFLAGS=$CXXFLAGS
+        export CCFLAGS=$CXXFLAGS
 
-		if [ ! -d "linux-$(uname -m)" ]; then
-			mkdir -p "linux-$(uname -m)"
-			mkdir -p "checkout/linux-$(uname -m)"
-			mkdir "build"
-		fi
+        if [ ! -d "linux-$(uname -m)" ]; then
+            mkdir -p "linux-$(uname -m)"
+        fi
+        if [ ! -d "checkout/linux-$(uname -m)" ]; then
+            mkdir -p "checkout/linux-$(uname -m)"
+        fi
 
-		echo -e "Building xulrunner 1.9.2...\n"
-		make -C xulrunner-1.9.2 -f Makefile.songbird xr-all
+        echo -e "Building xulrunner 1.9.2..."
+        make -C xulrunner-1.9.2 -f Makefile.songbird xr-all
 
-		echo -e "Building sqlite...\n"
-		make -C sqlite -f Makefile.songbird
-		
-		echo -e "Building taglib...\n"
-		make -C taglib -f Makefile.songbird
-		
-		echo -e "Done! Provided there were no errors, you can \nfind your deps in the linux-$(uname -m) directory. Copy or link it into [nightingale build directory]/dependencies and you're ready to build!\n"
-	;;
+        echo -e "Building sqlite..."
+        make -C sqlite -f Makefile.songbird
+
+        echo -e "Building taglib..."
+        make -C taglib -f Makefile.songbird
+
+        echo -e "Done! Provided there were no errors, you can \nfind your deps in the linux-$(uname -m) directory. Copy or link it into [nightingale build directory]/dependencies and you're ready to build!\n"
+    ;;
+
     darwin*)
-		# on OSX, we want 32 bit builds
-		arch_flags="-m32 -arch i386"
-		export CFLAGS="$arch_flags"
-		export CXXFLAGS="$arch_flags"
-		export CPPFLAGS="$arch_flags"
-		export LDFLAGS="$arch_flags"
-		export OBJCFLAGS="$arch_flags"
+        # on OSX, we want 32 bit builds
+        arch_flags="-m32 -arch i386"
+        export CFLAGS="$arch_flags"
+        export CXXFLAGS="$arch_flags"
+        export CPPFLAGS="$arch_flags"
+        export LDFLAGS="$arch_flags"
+        export OBJCFLAGS="$arch_flags"
 
-		if [ ! -d "macosx-i686" ]; then
-			mkdir -p "macosx-i686/mozilla-1.9.2/release/scripts"
-			cp xulrunner-1.9.2/mozilla/toolkit/crashreporter/tools/symbolstore.py macosx-i686/mozilla-1.9.2/release/scripts
-			mkdir -p "checkout/macosx-i686"
-			if [ ! -d "build" ]; then
-				mkdir "build"
-			fi
-		fi
+        if [ ! -d "macosx-i686" ]; then
+            mkdir -p "macosx-i686"
+        fi
+        if [ ! -d "checkout/macosx-i686" ]; then
+            mkdir -p "checkout/macosx-i686"
+        fi
 
         echo -e "Building gettext..."
         make CC=gcc CXX=g++ -C gettext -f Makefile.songbird
@@ -61,6 +62,10 @@ case $OSTYPE in
         make CC=gcc CXX=g++ -C glib -f Makefile.songbird
         echo -e "Building libidl..."
         make CC=gcc CXX=g++ -C libidl -f Makefile.songbird
+
+        echo -e "Building xulrunner and crossing our fingers..."
+        make CC=gcc-4.2 CXX=g++-4.2 -C xulrunner-1.9.2 -f Makefile.songbird xr-all
+
         echo -e "Building flac..."
         make CC=gcc CXX=g++ -C flac -f Makefile.songbird
         echo -e "Building libjpeg..."
@@ -78,8 +83,6 @@ case $OSTYPE in
         echo -e "Building taglib..."        
         make CC=gcc CXX=g++ -C taglib -f Makefile.songbird
 
-        echo -e "Building xulrunner and crossing our fingers..."
-        make CC=gcc-4.2 CXX=g++-4.2 -C xulrunner-1.9.2 -f Makefile.songbird xr-all
         echo -e "Building gstreamer bits..."
         make CC=gcc-4.2 CXX=g++-4.2 -C gstreamer -f Makefile.songbird
         echo -e "Building gst plugins...here's hoping these all build!"
@@ -90,16 +93,87 @@ case $OSTYPE in
         echo "Done!"
 
     ;;
-    msys*)
-		if [ ! -d "windows-i686-msvc8" ]; then
-			mkdir -p "windows-i686-msvc8"
-			mkdir -p "checkout/windows-i686-msvc8"
-			mkdir "build"
-		fi
 
-		make
+    msys*)
+        if [ ! -d "windows-i686-msvc8" ]; then
+            mkdir -p "windows-i686-msvc8"
+        fi
+        if [ ! -d "checkout/windows-i686-msvc8" ]; then
+            mkdir -p "checkout/windows-i686-msvc8"
+        fi
+
+        WIN_OUTPUT_DIR="$DIR/windows-i686-msvc8"
+        WIN_PB_DIR="$DIR/prebuilt/win32"
+
+        export DIRECTX_SDK_ROOT="/c/SDKs/DXSDK_Jun08"
+        export QUICKTIME_SDK_ROOT="/c/SDKs/QuickTime-SDK"
+        export WINDOWS_SDK_ROOT="/c/SDKs/Windows-SDK/v6.0"
+
+
+        echo -e "Extracting prebuilt libiconv package..."
+        pushd . > /dev/null
+        cd $WIN_PB_DIR
+        unzip libiconv-1.9.1-win32.zip
+        mv libiconv-1.9.1-win32 $WIN_OUTPUT_DIR/libiconv
+        popd > /dev/null
+
+        echo -e "Building gettext..."
+        make -C gettext-win -f Makefile.songbird
+
+        echo -e "Extracting prebuilt glib package..."
+        pushd . > /dev/null
+        cd $WIN_PB_DIR
+        unzip glib-2.34.3-win32.zip
+        mv glib-2.34.3-win32 $WIN_OUTPUT_DIR/glib
+        popd > /dev/null
+
+        # Need some scripts from XR to generate symbols in other deps,
+        # so build XR here
+        echo -e "Building xulrunner 1.9.2..."
+        make -C xulrunner-1.9.2 -f Makefile.songbird xr-all
+
+        echo -e "Building zlib..."
+        make -C zlib -f Makefile.songbird
+        echo -e "Building sqlite..."
+        make -C sqlite -f Makefile.songbird
+        echo -e "Building tablib..."
+        make -C taglib -f Makefile.songbird
+        echo -e "Building libjpeg-turbo..."
+        make -C libjpeg-turbo -f Makefile.songbird
+        echo -e "Building flac..."
+        make -C flac-win -f Makefile.songbird
+
+        echo -e "Building libogg..."
+        make -C libogg -f Makefile.songbird
+        echo -e "Building libvorbis..."
+        make -C libvorbis -f Makefile.songbird
+        echo -e "Building libtheora..."
+        make -C libtheora -f Makefile.songbird
+
+        echo -e "Extracting prebuilt bins package..."
+        pushd . > /dev/null
+        cd $WIN_PB_DIR
+        unzip win32-bins-20140810.zip
+        popd > /dev/null
+        echo -e "Adding bins to PATH..."
+        WIN_PB_BINS_DIR=$WIN_PB_DIR/win32-bins-20140810
+        export PATH=$WIN_PB_BINS_DIR:$PATH
+
+        echo -e "Building gstreamer..."
+        make -C gstreamer -f Makefile.songbird
+
+        echo -e "Building gst-plugins-base..."
+        make -C gst-plugins-base -f Makefile.songbird
+        echo -e "Building gst-plugins-good..."
+        make -C gst-plugins-good -f Makefile.songbird
+        echo -e "Building gst-plugins-bad..."
+        make -C gst-plugins-bad -f Makefile.songbird
+        echo -e "Building gst-plugins-ugly..."
+        make -C gst-plugins-ugly -f Makefile.songbird
+
+        echo "Done!"
     ;;
-	*)
-		echo "Lazy buildscript for your OS coming soon."
-	;;
+    *)
+      echo "Lazy buildscript for your OS coming soon."
+    ;;
 esac
