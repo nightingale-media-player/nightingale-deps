688c0d15e69fac6df928dd17864ddda445ade0be
Matt Parnell <parwok@gmail.com>
2015-10-01 01:34:46 -0500

    retry autogen - mac isn't playing nice

--- build.sh
+++ build.sh
@@ -56,6 +56,9 @@ case $OSTYPE in
 		fi
 
         echo -e "Building gettext..."
+        cd gettext
+        bash autogen.sh
+        cd ..
         make CC=gcc CXX=g++ -C gettext -f Makefile.songbird
         
         echo -e "Building glib..."
